26bbb530a116a268fe2c4b06deec02414f0053c9
aquifzubair <aquifzubair98@gmail.com>
2020-06-14 04:10:07 +0530

    minor changes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,17 +4,19 @@ import * as ReactNavbar from 'react-bootstrap';
 const Navigation = () => {
     return (
         <ReactNavbar.Container>
-            <ReactNavbar.Navbar collapseOnSelect expand="md" fixed='top' className='color-nav' style={{ marginBottom: '-40px' }}>
+            <ReactNavbar.Navbar collapseOnSelect expand="sm" fixed='top' className='color-nav' style={{ marginBottom: '-40px' }}>
                 <ReactNavbar.Navbar.Brand href="#home" style={{ fontSize: '20px', paddingTop: '10px', fontWeight:'bold', color:'white' }}>Home</ReactNavbar.Navbar.Brand>
                 <ReactNavbar.Navbar.Toggle aria-controls="responsive-ReactNavbar.Navbar-nav" />
                 <ReactNavbar.Navbar.Collapse id="responsive-ReactNavbar.Navbar-nav">
                     <ReactNavbar.Nav className="mr-auto color-link">
                         <ReactNavbar.Nav.Link href="#aboutMe" id='color-link' >AboutMe</ReactNavbar.Nav.Link>
                         <ReactNavbar.Nav.Link href="#project" id='color-link'>Projects</ReactNavbar.Nav.Link>
-                            <ReactNavbar.Nav.Link href="#skills" id='color-link'>My Skills</ReactNavbar.Nav.Link>
-                            <ReactNavbar.Nav.Link href="#internship" id='color-link'>Experience</ReactNavbar.Nav.Link>
-                            <ReactNavbar.Nav.Link href="#education" id='color-link'>Education</ReactNavbar.Nav.Link>
-                            <ReactNavbar.Nav.Link href="#language" id='color-link'>Language</ReactNavbar.Nav.Link>
+                        <ReactNavbar.NavDropdown title="More" id="collasible-nav-dropdown " style = {{fontSize: '15px'}}>
+                            <ReactNavbar.NavDropdown.Item href="#skills" >My Skills</ReactNavbar.NavDropdown.Item>
+                            <ReactNavbar.NavDropdown.Item href="#internship" >Experience</ReactNavbar.NavDropdown.Item>
+                            <ReactNavbar.NavDropdown.Item href="#education" >Education</ReactNavbar.NavDropdown.Item>
+                            <ReactNavbar.NavDropdown.Item href="#language">Language</ReactNavbar.NavDropdown.Item>
+                        </ReactNavbar.NavDropdown>
                     </ReactNavbar.Nav>
                     <ReactNavbar.Nav>
                         <ReactNavbar.Nav.Link href="#socialNetwork" id='color-link'>Social Network</ReactNavbar.Nav.Link>
